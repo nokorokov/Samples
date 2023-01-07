d96781dccacc7cef38be72dad277beb577802128
nokorokov <n.okorokov@itpelag.com>
2023-01-07 18:35:36 +0300

    adding fixtures and pytest assert

---
+++ test/test_pytest_fixture1.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+import pytest
+from selenium.webdriver.common.by import By
+
+link = "http://selenium1py.pythonanywhere.com/"
+
+
+@pytest.fixture()
+def browser():
+    print("\nStart browser for test..")
+    browser = webdriver.Chrome()
+    return browser
+
+
+class TestMainPage1():
+
+    def test_guest_should_see_login_link(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
+        browser.get(link)
+        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+
+
